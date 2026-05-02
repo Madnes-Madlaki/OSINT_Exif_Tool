c35e9c64565269b4c54a0bd6b11fff9511b027b1
Hakim Mallaki <hakimmallaki.pro@gmail.com>
2026-05-02 02:48:50 +0100

    added google maps link creation if gps metadata is present || added json file creation after exif extraction

--- extractScript.py
+++ extractScript.py
@@ -1,8 +1,16 @@
 from exif import Image
 import sys
+import json
+import datetime
+
+metadata_dictionary = {}
 
 def extractionFunc(imagePath):
     try :
+        latitude = None
+        longitude = None
+        latitude_ref = None
+        longitude_ref = None
         with open(imagePath, 'rb') as imageFile:  
             #as imageFile takes the user data and converts it into something that the exif func can understand
             #then store it in a new var
@@ -14,11 +22,53 @@ def extractionFunc(imagePath):
                 for metadataName, metadataValue in userImage.get_all().items():
                     #iterate through the metadata to get each field and its value, date : 01/01/2000 for example
                     print(f"{metadataName} : {metadataValue}")
-            
+
+                    #storing each metadata in a dictionary
+                    metadata_dictionary[metadataName] = str(metadataValue)
+
+                    #creating the google maps link section
+                    if metadataName == 'gps_latitude':
+                        latitude = metadataValue
+                    elif  metadataName == 'gps_latitude_ref':
+                        latitude_ref = metadataValue
+
+                    elif metadataName == 'gps_longitude':
+                        longitude = metadataValue
+                    elif metadataName == 'gps_longitude_ref':
+                        longitude_ref = metadataValue
+
+                if latitude and latitude_ref and longitude and longitude_ref:
+                    latitude_degrees, latitude_minutes, latitude_secondes = latitude
+                    longitude_degrees, longitude_minutes, longitude_secondes = longitude
+
+                    latitude_decimal = gpsMetadata_intoDecimal(latitude_degrees, latitude_minutes, latitude_secondes, latitude_ref)
+                    longitude_decimal = gpsMetadata_intoDecimal(longitude_degrees, longitude_minutes, longitude_secondes, longitude_ref)
+
+                    link = f"Google Maps : https://www.google.com/maps?q={latitude_decimal},{longitude_decimal}"
+
+                    metadata_dictionary['google_mapsLink'] = link
+
+                else: print("no gps data found")
+
+                 #Dictionnary contunuity section, open a json file and store what we extracted into the dictionary in it
+                outputFile = f"metadata_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.json"
+                with open(outputFile, 'w') as fileVar:
+                    json.dump(metadata_dictionary, fileVar, indent=4)
+                print("metadata has been saved into a json file")
+                
+                return link
+
             else: print("the image has been stripped away from its metadata\n")
 
     except Exception as error:
         print(f"some kind of error occured, please check the path of the image/file || {error}\n" )
+
+def gpsMetadata_intoDecimal(degrees, minutes, seconds, ref):
+    decimal = degrees + minutes / 60 + seconds / 3600
+    if ref in ['S', 'W']:
+        decimal = -decimal
+    return decimal
+    #the exif library gives the coordinates in DMS format, we need to handle this within the function
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -29,9 +79,14 @@ if __name__ == "__main__":
 
 imagePath = sys.argv[1]
 #argv[0] is the name of the script, argv[1] is the path of the image
-print("wait for extraction")
-print(f"analyzing path : {imagePath}")
-extractionFunc(imagePath)
+print("*"*50)
+print("wait for extraction\n")
+print(f"analyzing path : {imagePath}\n")
+print("*"*50)
+link = extractionFunc(imagePath)
+print("\n" + "*"*50)
+print(f"{link}")
+print("*"*50)
 
 
 
